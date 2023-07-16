d106aafc356c62b82bf9c469ecdf66a376acfbdd
Foad Botan <foadbotan@gmail.com>
2023-07-16 23:33:19 +1000

    Create "edit" events functionality

--- routes/events.py
+++ routes/events.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from models import Event
 
 events_bp = Blueprint("events", __name__, url_prefix="/events")
@@ -14,3 +14,21 @@ def events_list():
 def event_detail(event_id=None):
     event = Event.get_by_id(event_id)
     return render_template("event_detail.html.jinja", event=event)
+
+
+@events_bp.get("/edit/<int:event_id>")
+def event_edit_form(event_id=None):
+    event = Event.get_by_id(event_id)
+    return render_template(
+        "forms/event.html.jinja",
+        event=event,
+        title=f"Edit {event.name}",
+        button_text="Save Changes",
+    )
+
+
+@events_bp.post("/edit/<int:event_id>")
+def event_edit_action(event_id=None):
+    event = Event.get_by_id(event_id)
+    event.update(**request.form)
+    return redirect(f"/events/{event.id}")
